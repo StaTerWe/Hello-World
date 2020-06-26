73865b1ad639fc74b27c607aa4184cc936599941
Tomás Bergonzi <tomi.bergonzi@gmail.com>
2020-06-26 16:29:26 -0300

    agrega index.html

--- index.js
+++ index.js
@@ -3,8 +3,14 @@
 // importa la libreria express
 const express = require('express');
 
+// ponemos el puerto del server en una variable
+const port = process.env.PORT || 3000;
+
 // crea el objeto app
 const app = express();
+
+// agregamos esta línea
+app.use(express.static('public'));
 
 // la app responde con Hello world
 // a todas las peticiones GET a /
@@ -13,4 +19,4 @@ app.get('/', (req, res) => {
 });
 
 // el server escucha en el puerto 3000
-app.listen(3000);
+app.listen(port);
